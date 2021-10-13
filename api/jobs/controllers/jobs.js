0dc5fbe0749c8d9963136b56adf614fad05f94ed
Ademayowa <ademayowaadeniyi@gmail.com>
2021-10-13 13:08:50 +0100

    created custom backend endpoint for strapi admin users

--- api/jobs/controllers/jobs.js
+++ api/jobs/controllers/jobs.js
@@ -1,8 +1,30 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // Get logged in users
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    // Get user by token
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    // Gets a user specific job
+    const data = await strapi.services.jobs.find({ job: user.id });
+
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    return sanitizeEntity(data, { model: strapi.models.jobs });
+  },
+};
