ad41b64f2187b3dbeace924edd2793da98a3c3a4
Ademayowa <ademayowaadeniyi@gmail.com>
2021-10-15 12:46:04 +0100

    ensure a user is not able to edit/delete another user's job (owner policy in strapi)

--- api/jobs/controllers/jobs.js
+++ api/jobs/controllers/jobs.js
@@ -7,6 +7,65 @@ const { sanitizeEntity } = require("strapi-utils");
  */
 
 module.exports = {
+  // Create job with linked user
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.job = ctx.state.user.id;
+      entity = await strapi.services.jobs.create(data, { files });
+    } else {
+      ctx.request.body.job = ctx.state.user.id;
+      entity = await strapi.services.jobs.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.jobs });
+  },
+
+  // Update job
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [jobs] = await strapi.services.jobs.find({
+      id: ctx.params.id,
+      "job.id": ctx.state.user.id,
+    });
+
+    if (!jobs) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.jobs.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.jobs.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.jobs });
+  },
+
+  // Delete job
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [jobs] = await strapi.services.jobs.find({
+      id: ctx.params.id,
+      "job.id": ctx.state.user.id,
+    });
+
+    if (!jobs) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    const entity = await strapi.services.jobs.delete({ id });
+
+    return sanitizeEntity(entity, { model: strapi.models.jobs });
+  },
+
   // Get logged in users
   async me(ctx) {
     const user = ctx.state.user;
